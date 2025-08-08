88a3beed89bdbcc1d9d7359e70b3fdcb965df790
rvsshasank-dev <rvsshasank@gmail.com>
2025-08-08 00:47:25 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://c1os4xvwji.execute-api.us-east-1.amazonaws.com/default/myfirstlambda'
+const API_ENDPOINT = ' https://xceg9rcc37.execute-api.us-east-1.amazonaws.com/default/serverless-function'
 
 export default {
   name: 'app',
